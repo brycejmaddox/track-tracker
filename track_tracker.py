a8ce6d047d02f4e2353638960b4e41b7e341d3b9
Bryce Maddox <brycejmaddox@gmail.com>
2026-08-02 00:18:46 -0400

    Refactor time calculation functions for clarity and consistency

--- track_tracker.py
+++ track_tracker.py
@@ -2,42 +2,51 @@ times = []
 loop = True
 event = input("Event:")
 def find_fastest(times):
-    fastest = times[0]
-    for i in range(len(times)):
-        if times[i] < fastest:
-            fastest = times[i]
-    return fastest
+    try:
+        fastest = times[0]
+        for i,value in enumerate(times):
+            if value < fastest:
+                fastest = value
+        return fastest
+    except IndexError:
+        return "No times recorded yet"
 def find_slowest(times):
-    slowest = times[0]
-    for i in range(len(times)):
-        if times[i] > slowest:
-            slowest = times[i]
-    return slowest
+    try:
+        slowest = times[0]
+        for i,value in enumerate(times):
+            if value > slowest:
+                slowest = value
+        return slowest
+    except IndexError:
+        return "No times recorded yet"
 def average(times):
-    avg = sum(times) / len(times)
-    return avg
+    try:
+        avg = sum(times) / len(times)
+        return avg
+    except ZeroDivisionError:
+        return "No times recorded yet"
 while loop:
     goal = input("What would you like to do (add/remove/replace time, find fastest/slowest time, average of times)? If you are done, type 'Done' or 'Thank you': ").lower()
     if goal == "add time":
         time = float(input("Time:"))
         times.append(time)
         print("New list of times:")
-        for i in range(len(times)):
-            print(1 + i, times[i])
+        for i,value in enumerate(times):
+            print(1 + i, value)
     elif goal == "remove time":
         time = float(input("Time:"))
         times.remove(time)
         print("New list of times:")
-        for i in range(len(times)):
-            print(1 + i, times[i])
+        for i,value in enumerate(times):
+            print(1 + i, value)
     elif goal == "replace time":
         time1 = float(input("Time to remove:"))
         times.remove(time1)
         time2 = float(input("Time to add:"))
         times.append(time2)
         print("New list of times:")
-        for i in range(len(times)):
-            print(1 + i, times[i])
+        for i,value in enumerate(times):
+            print(1 + i, value)
     elif goal == "find fastest time":
         print(f"Fastest {event} time:", find_fastest(times))
     elif goal == "find slowest time":
